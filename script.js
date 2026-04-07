8b26ab993df5ca06be115821986704799f91eec0
Teddy Mulyana <42166005+ayahelang@users.noreply.github.com>
2026-04-07 21:59:26 +0700

    Add files via upload

--- script.js
+++ script.js
@@ -1,3 +1,14 @@
-function contactWA(){
-  window.open("https://wa.me/6285158822803","_blank");
+function contactWA() {
+    window.open("https://wa.me/6285158822803", "_blank");
 }
+
+/* smooth scroll */
+document.querySelectorAll("a").forEach(anchor => {
+    anchor.addEventListener("click", function (e) {
+        if (this.getAttribute("href").startsWith("#")) {
+            e.preventDefault();
+            document.querySelector(this.getAttribute("href"))
+                .scrollIntoView({ behavior: "smooth" });
+        }
+    });
+});
